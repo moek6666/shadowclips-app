bd0fb75da019f89739f59782e5f2e72c86b33f28
unknown <tiktiktoktok00@gmail.com>
2026-08-05 07:48:25 +0700

    chore: menaikkan batas ukuran chunk vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Menaikkan batas peringatan menjadi 2000 KB (2 MB)
+  }
 })
