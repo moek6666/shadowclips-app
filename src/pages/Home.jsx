6e1260fb290777816efa50f5c2f13daff003414d
unknown <tiktiktoktok00@gmail.com>
2026-09-06 23:42:36 +0700

    Change Selogan Website In Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -111,7 +111,7 @@ export default function Home({ supabase }) {
                     {currentPage === 1 ? (
                         <h1 className="text-zinc-600 dark:text-zinc-400 text-sm md:text-base leading-relaxed transition-colors">
                             <strong className="text-zinc-900 dark:text-white font-black text-lg md:text-xl tracking-tight mr-2 transition-colors">Shadow<span className="text-[#106EBE]">Clips</span></strong>
-                            is an exclusive, viral, comprehensive, and updated video streaming hub. Enjoy unlimited premium entertainment collections with the best viewing quality every day.
+                            Selamat Datang! Shadowclips.asia Situs Streaming Video Bokep HD Global , Dan Tentunya Watermark Friendly, Selamat Bergabung Di Komunitas.
                         </h1>
                     ) : (
                         <h1 className="text-zinc-600 dark:text-zinc-400 text-sm md:text-base leading-relaxed transition-colors">
